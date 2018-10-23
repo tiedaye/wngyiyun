e0f96251c3eb2cc487663ab80bd05af594f7afd6
zhangshuang-h <781792980@qq.com>
2018-10-23 08:52:30 +0800

    建立组件

---
+++ wangyiapp/src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+    <svg class="icon" aria-hidden="true">
+        <use xlink:href="#icon-wangyiyun"></use>
+    </svg>
+</template>
+
+<script src="../assets/logo/iconfont.js">
+
+    export default {
+        name: "home"
+    }
+</script>
+
+<style scoped type="text/css">
+    .icon {
+        width: 1em; height: 1em;
+        vertical-align: -0.15em;
+        fill: currentColor;
+        overflow: hidden;
+    }
+</style>
